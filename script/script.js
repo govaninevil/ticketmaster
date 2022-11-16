a3476acdb4b89c38bea369f74b7b68f4211639b3
MR. Golu <88421428+govaninevil@users.noreply.github.com>
2022-11-15 22:52:01 -0800

    main

--- script/script.js
+++ script/script.js
@@ -2,6 +2,7 @@ $(document).on('ready', function () {
 
     $('#event-slider').slick({
         dots: false,
+        margin: 10,
         infinite: true,
         slidesToShow: 6,
         slidesToScroll: 1,
@@ -14,6 +15,89 @@ $(document).on('ready', function () {
             breakpoint: 1024,
             settings: {
               slidesToShow: 8,
+              slidesToScroll: 1,
+              infinite: true,
+              dots: true
+            }
+          },
+          {
+            breakpoint: 600,
+            settings: {
+              slidesToShow: 4,
+              slidesToScroll: 1
+            }
+          },
+          {
+            breakpoint: 480,
+            settings: {
+              slidesToShow: 3,
+              slidesToScroll: 1,
+              autoplay: true,
+              autoplaySpeed: 1000
+            }
+          }
+          // You can unslick at a given breakpoint now by adding:
+          // settings: "unslick"
+          // instead of a settings object
+        ]
+      });
+
+
+      $('#ltr-slider').slick({
+        dots: false,
+        infinite: true,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed:0,
+        speed:8000,
+        cssEase: 'linear',
+        responsive: [
+          {
+            breakpoint: 1024,
+            settings: {
+              slidesToShow: 4,
+              slidesToScroll: 1,
+              infinite: true,
+              dots: true
+            }
+          },
+          {
+            breakpoint: 600,
+            settings: {
+              slidesToShow: 4,
+              slidesToScroll: 1
+            }
+          },
+          {
+            breakpoint: 480,
+            settings: {
+              slidesToShow: 3,
+              slidesToScroll: 1,
+              autoplay: true,
+              autoplaySpeed: 1000
+            }
+          }
+          // You can unslick at a given breakpoint now by adding:
+          // settings: "unslick"
+          // instead of a settings object
+        ]
+      });
+
+      $('#rtl-slider').slick({
+        dots: false,
+        infinite: true,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed:0,
+        speed:8000,
+        cssEase: 'linear',
+        responsive: [
+          {
+            breakpoint: 1024,
+            settings: {
+              slidesToShow: 4,
               slidesToScroll: 1,
               infinite: true,
               dots: true
